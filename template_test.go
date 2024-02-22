a306a1b725220f405d1dc5cd108a2a271365dc3f
Bruno Schaatsbergen <git@bschaatsbergen.com>
2024-02-22 17:10:44 +0100

    chore: improve template

--- template_test.go
+++ template_test.go
@@ -3,15 +3,15 @@ package template_test
 import (
 	"testing"
 
-	template "github.com/go-random/template"
+	number "github.com/go-random/template"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestGenerateRandomNumber(t *testing.T) {
+func TestInt(t *testing.T) {
 	// Setup
-	randomizer := template.NewRandomizer()
-	randomNumber := randomizer.GenerateRandomNumber()
+	randomizer := number.NewRandomizer()
+	randomNumber := randomizer.Int()
 
 	// Assert
-	assert.NotEqual(t, 0, randomNumber, "Generated number should not be 0")
+	assert.NotEqual(t, 0, randomNumber, "randomized signed integer should not be 0")
 }
